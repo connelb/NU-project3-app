291141713958cdc075ed7cc94de86093f17ca185
Brian Connell <brianconnell@brians-mbp.attlocal.net>
2018-10-01 21:42:38 -0500

    adding bootstrap

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def prepare_image(img):
 def upload_file():
     data = {"success": False}
     if request.method == 'POST':
-        print(request)
+        # print(request)
 
         if request.files.get('file'):
             # read the file
@@ -63,7 +63,7 @@ def upload_file():
 
             # Convert the 2D image to an array of pixel values
             image_array = prepare_image(im)
-            print(image_array)
+            # print(image_array)
 
             # Get the tensorflow default graph and use it to make predictions
             global graph
@@ -75,19 +75,43 @@ def upload_file():
 
                 # indicate that the request was a success
                 data["success"] = True
+                return '''
+                <!doctype html>
+                            <html lang="en">
 
-            return jsonify(data)
+                            <head>
+                            <!-- Required meta tags -->
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+                            <!-- Bootstrap CSS -->
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+                                crossorigin="anonymous">
+
+                            <title>Results:</title>
+                            </head>
+
+                            <body>
+                            <div class="jumbotron">
+                            <a href="" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Home</a>
+                            <br>
+                            <br>
+                                <h1 class="display-4">Analysis Results:</h1>
+                                <p class="lead"><strong>Outcome:</strong> '''+str(data["success"])+'''</p>
+                                <p class="lead"><strong>Probability of detection:</strong> '''+str(data["prediction"])+'''</p>
+                                
+                            </div>
+                            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                                crossorigin="anonymous"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+                                crossorigin="anonymous"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+                                crossorigin="anonymous"></script>
+                            </body>
+                        </html>
+                '''
+
     return render_template('index.html')
-    # return '''
-    # <!doctype html>
-    # <title>Upload new File</title>
-    # <h1>Upload new File</h1>
-    # <form method=post enctype=multipart/form-data>
-    #   <p><input type=file name=file>
-    #      <input type=submit value=Upload>
-    # </form>
-    # '''
-
 
 if __name__ == "__main__":
     app.run()
